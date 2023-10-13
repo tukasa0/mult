2de0968c1146669289f3be9b5249c360f41de139
sk2k0 <sk2k0@MSI>
2023-10-13 12:06:54 +0900

    関数から呼び出そうとした

--- 呼び出し用.cpp
+++ 呼び出し用.cpp
@@ -3,12 +3,11 @@ using namespace std;
 
 int mult(int value1, int value2);
 
-
-int main()
+void Show()
 {
 	int value1 = 2;
 	int value2 = 3;
 
+	//呼び出せないからエラーが出る
 	cout << mult(value1, value2) << endl;
-
 }
