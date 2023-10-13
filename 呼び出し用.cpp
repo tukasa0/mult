ca4ec3a0bf6e6c8b30cffc1347fb6dd941a6e1ce
sk2k0 <sk2k0@MSI>
2023-10-13 11:58:56 +0900

    プロジェクト ファイルを追加します。

---
+++ 呼び出し用.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int mult(int value1, int value2);
+
+
+int main()
+{
+	int value1 = 2;
+	int value2 = 3;
+
+	cout << mult(value1, value2) << endl;
+
+}
---
+++ 教科書確認問題3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+static int mult(int value1, int value2)
+{
+	return value1 * value2;
+}
+
+//int main()
+//{
+//	int value1 = 2;
+//	int value2 = 3;
+//
+//	cout << mult(value1, value2) << endl;
+//
+//}
